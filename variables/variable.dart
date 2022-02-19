60474e71559375127fc6a84c2a719b78197bf0f9
Gabriel Donato <gabrielcdonato@gmail.com>
2022-02-19 19:09:31 -0300

    Feat:(DART) how to create variables

---
+++ variables/variable.dart
@@ -0,0 +1,11 @@
+// how to create your variable your need this steps:
+//type /name/ instance what your want
+var first_variable; //bad pratice
+var firstVariable = "Hello World"; //good pratice
+var secondVariable;
+
+void main() {
+  // secondVariable = "hi";
+  secondVariable = -10;
+  print(secondVariable);
+}
